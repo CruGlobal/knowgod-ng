289225f983acec892901a2dac4319748ea908f77
Carl Hempel <cshempel@gmail.com>
2018-04-23 16:03:49 -0400

    - attempted start into ng-route to give us page urls - got manifest lookup functional - correctly imports image urls from the manifest file. - improved formatting of index.html

--- controller.js
+++ controller.js
@@ -1,4 +1,15 @@
-angular.module('knowGod', [])
+angular.module('knowGod', ['ngRoute'])
+  .config( function ($locationProvider, $httpProvider, $qProvider, $routeProvider) {
+    $locationProvider.html5Mode(true).hashPrefix('');
+    $httpProvider.useApplyAsync(true);
+
+/*    $routeProvider
+      .when('/en', {
+        title: 'KnowGod.com'
+      })*/
+      
+
+  })
   .factory('page', function ($http, $q) {
     var service = {};
     var baseUrl = 'http://0.0.0.0:8000/knowGodResource/';
@@ -59,68 +70,50 @@ angular.module('knowGod', [])
     service.loadManifest = function () {
       makeUrl();
       var deferred = $q.defer();
-      $http.get(_finalUrl, 
+      $http.get(_finalUrl/*, 
       {
         transformResponse: function (cnv) {
           var x2js = new X2JS();
           var aftCnv = x2js.xml_str2json(cnv);
           return aftCnv;
         }
-      })
+      }*/)
       .then(function(data) {
         deferred.resolve(data);
-        _manifest = data.data;
+        _manifest = $(data.data);
       })
       return deferred.promise;
     }
 
     service.lookup = function (filename) {
-
-      var deferred = $q.defer();
-
-      var dothething = function () {
-        if (_manifest == '') {
-          service.loadManifest()
-            .then(function (data) {
-              $window.alert(JSON.stringify(_manifest));
-
-              deferred.resolve(data);
-
-              return _manifest;
-            }, function (data) {
-              return $window.alert(data);
-            })
-        }
-        else {
-          $window.alert(JSON.stringify(_manifest));   
-        }
-      }
-      //return dothething();
-      return deferred.promise;
+      var src = _manifest.find("[filename=\""+filename+"\"]").attr("src");
+      return baseUrl+src;
+    }
+    service.percent = function () {
+      return _manifest.find('page').length;
     }
     return service;
   })
   .controller('KnowGodController', function($scope, $http, $window, page, manifest) {
-
     var knowGod = this;
-    knowGod.todos = [
-      {text:'learn AngularJS', done:true},
-      {text:'build an AngularJS app', done:false}];
 
-    page.setUrl('743fa53e8470cd67e1ca12ea05fbd4bd64dea08b7326691cbd888b107a2836ce.xml');
+    manifest.loadManifest().then(function(data){
+      knowGod.manifest = manifest;
+    })
+
+//    page.setUrl('743fa53e8470cd67e1ca12ea05fbd4bd64dea08b7326691cbd888b107a2836ce.xml');
+    page.setUrl('f1165b62b93e61461f64446b3198c3a9245c9f784c1fcf25efa7fd71b85e21f0.xml');
+//    page.setUrl('908a4c4c092d97db3f3c7b59a3fdb03ecf4da204bfd3f283568b7a2614635ee4.xml');
+//    page.setUrl('90feba69d384d8d99f67f7ad024797577c3ca46be73a3c0f7928b63fc316669e.xml');
     knowGod.loadPage = function () {
       page.loadPage()
         .then(function (data) {
           knowGod.page = data.data.page;
         }, function (data) {
-          $window.alert(data);
         })
     }
     knowGod.loadPage();
-    manifest.lookup('01_Home.xml').then(function(data){
-      console.log(data);
 
-    })
 
     var languages = function() {
       var url = 'https://mobile-content-api.cru.org/languages/';
