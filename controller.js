c8b4f20ae5fe7b78cc563b24bad70275f2430fd1
Carl Hempel <cshempel@gmail.com>
2018-04-26 16:31:30 -0400

    Navigation via buttons on the bottom now functional.

--- controller.js
+++ controller.js
@@ -10,44 +10,7 @@ angular.module('knowGod', ['ngRoute'])
       
 
   })
-  .factory('page', function ($http, $q) {
-    var service = {};
-    var baseUrl = 'http://0.0.0.0:8000/knowGodResource/';
-    var _url = '';
-    var _finalUrl = '';
-
-    var makeUrl = function () {
-      _finalUrl = baseUrl + _url;
-      return _finalUrl;
-    }
-
-    service.setUrl = function (url) {
-      _url = url;
-    }
-
-    service.getUrl = function () {
-      return _url;
-    }
-
-    service.loadPage = function () {
-      makeUrl();
-      var deferred = $q.defer();
-      $http.get(_finalUrl, 
-      {
-        transformResponse: function (cnv) {
-          var x2js = new X2JS();
-          var aftCnv = x2js.xml_str2json(cnv);
-          return aftCnv;
-        }
-      })
-      .then(function(data) {
-        deferred.resolve(data);
-      })
-      return deferred.promise;
-    }
-    return service;
-  })
-  .factory('manifest', function ($http, $q, $window) {
+  .factory('manifest', function ($http, $q, $window, page) {
     var service = {};
     var baseUrl = 'http://0.0.0.0:8000/knowGodResource/';
     var _url = '392380f776ebdffe4a0fd286e522d5cad5930f0b14db0554debf409bc7218c3a.xml';
@@ -67,6 +30,7 @@ angular.module('knowGod', ['ngRoute'])
       return _url;
     }
 
+   //load tool
     service.loadManifest = function () {
       makeUrl();
       var deferred = $q.defer();
@@ -92,6 +56,70 @@ angular.module('knowGod', ['ngRoute'])
     service.percent = function () {
       return _manifest.find('page').length;
     }
+
+   //Navigate tool
+    service.nextPage = function () {
+      var nextUrl = _manifest.find("[src=\""+page.getUrl()+"\"]").next().attr("src")
+      if(nextUrl) {
+        page.setUrl(nextUrl);
+        page.loadPage();  
+      }
+      else {
+        console.log('end of pages');
+      }
+      console.log(page.getUrl());
+    }
+    service.prevPage = function () {
+      var prevUrl = _manifest.find("[src=\""+page.getUrl()+"\"]").prev().attr("src")
+      if(prevUrl) {
+        page.setUrl(prevUrl);
+        page.loadPage();  
+      }
+      else {
+        console.log('beg of pages');
+      }
+      console.log(page.getUrl());
+    }
+    
+    return service;
+  })
+  .factory('page', function ($http, $q) {
+    var service = {};
+    var baseUrl = 'http://0.0.0.0:8000/knowGodResource/';
+    var _url = '';
+    var _finalUrl = '';
+
+    var makeUrl = function () {
+      _finalUrl = baseUrl + _url;
+      return _finalUrl;
+    }
+
+    service.setUrl = function (url) {
+      _url = url;
+    }
+
+    service.getUrl = function () {
+      return _url;
+    }
+
+    service.content = {};
+    service.loadPage = function () {
+      makeUrl();
+//      var deferred = $q.defer();
+      $http.get(_finalUrl, 
+      {
+        transformResponse: function (cnv) {
+          var x2js = new X2JS();
+          var aftCnv = x2js.xml_str2json(cnv);
+          return aftCnv;
+        }
+      })
+      .then(function(data) {
+  //      deferred.resolve(data);
+        service.content = data.data.page;
+      })
+   //   return deferred.promise;
+    }
     return service;
   })
   .controller('KnowGodController', function($scope, $http, $window, page, manifest) {
@@ -101,8 +129,8 @@ angular.module('knowGod', ['ngRoute'])
       knowGod.manifest = manifest;
     })
 
-//    page.setUrl('743fa53e8470cd67e1ca12ea05fbd4bd64dea08b7326691cbd888b107a2836ce.xml');
-    page.setUrl('f1165b62b93e61461f64446b3198c3a9245c9f784c1fcf25efa7fd71b85e21f0.xml');
+    page.setUrl('743fa53e8470cd67e1ca12ea05fbd4bd64dea08b7326691cbd888b107a2836ce.xml');
+//    page.setUrl('f1165b62b93e61461f64446b3198c3a9245c9f784c1fcf25efa7fd71b85e21f0.xml');
 //    page.setUrl('908a4c4c092d97db3f3c7b59a3fdb03ecf4da204bfd3f283568b7a2614635ee4.xml');
 //    page.setUrl('90feba69d384d8d99f67f7ad024797577c3ca46be73a3c0f7928b63fc316669e.xml');
     knowGod.loadPage = function () {
@@ -112,7 +140,8 @@ angular.module('knowGod', ['ngRoute'])
         }, function (data) {
         })
     }
-    knowGod.loadPage();
+    page.loadPage();
+    knowGod.page = page;
 
 
     var languages = function() {
